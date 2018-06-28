fdd5694f7239a0a390fbb3a8a48c04cc381121d4
Victor <vicwong415@gmail.com>
2018-06-28 07:26:10 +0000

    update paths to be relative

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
 # sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/worlds/myWorld " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${DIR}/../worlds/myWorld " &
 sleep 5
 xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 5
--- scripts/wall_follower.sh
+++ scripts/wall_follower.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # xterm  -e  " source /opt/ros/kinetic/setup.bash; roscore" & 
 # sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/worlds/myWorld " &
+xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${DIR}/../worlds/myWorld " &
 sleep 5
-xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file:=/home/workspace/catkin_ws/src/gmapping.launch.xml" &
+xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file:=${DIR}/../gmapping.launch.xml" &
 sleep 5
 # xterm  -e  " roslaunch turtlebot_teleop keyboard_teleop.launch" &
 xterm -e " rosrun wall_follower wall_follower" &
